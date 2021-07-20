928d95dac1b137e22c107a6a3dfd1ae2e307c5d7
Tom Skelley <tom.skelley@digital.justice.gov.uk>
2021-07-20 15:42:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This repo contains tools to aid with GitHub Actions deployments.
+
+Initially it contains a modified copy of parse_deploy_file.json lifted from common concourse tools repo.
---
+++ parse_deploy_file.py
@@ -0,0 +1,44 @@
+import os
+import json
+
+lookup = {
+    '102PF Wifi': os.environ.get('IPS_102PF_WIFI'),
+    'Digital Wifi and VPN': os.environ.get('IPS_DIGITAL'),
+    'DOM1': os.environ.get('IPS_DOM1'),
+    'QUANTUM': os.environ.get('IPS_QUANTUM'),
+    'Alan Turing Institute': os.environ.get('IPS_TURING'),
+    'MoJo': os.environ.get('IPS_MOJO'),
+    'Any': ''
+}
+
+# with open('webapp-source/deploy.json') as input:
+with open('deploy.json') as input:
+    print('Parsing deploy.json')
+    data = json.load(input)
+
+allowed = data.get('allowed_ip_ranges', [])
+if 'Any' in allowed:
+    ip_ranges = ['']
+else:
+    ip_ranges = [lookup[name] for name in allowed if name in lookup]
+if not ip_ranges:
+    ip_ranges = [lookup['DOM1']]
+
+auth_required = not data.get('disable_authentication', False)
+webapp_port = data.get('port', 80)
+
+health_check = data.get('health_check', '/?healthz')
+
+# The following are input values of the webapp helm chart
+with open('deploy-params/overrides.yaml', 'w') as output:
+    json.dump({
+        'AuthProxy': {
+            'AuthenticationRequired': auth_required,
+            'IPRanges': ','.join(ip_ranges),
+        },
+        'WebApp': {
+            'Port': webapp_port,
+            'HealthCheck': health_check,
+        }
+    }, output)
+print('Wrote deploy-params/overrides.yaml')
